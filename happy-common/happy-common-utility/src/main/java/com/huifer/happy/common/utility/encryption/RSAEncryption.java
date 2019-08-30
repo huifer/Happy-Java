59cfd0d2976d680c3657d36f1fc7abf020989331
huifer <huifer97@163.com>
2019-08-30 08:55:21 +0800

    添加文档: 开发方向
    Signed-off-by: huifer <huifer97@163.com>

---
+++ happy-common/happy-common-utility/src/main/java/com/huifer/happy/common/utility/encryption/RSAEncryption.java
@@ -0,0 +1,197 @@
+package com.huifer.happy.common.utility.encryption;
+
+import org.apache.commons.codec.binary.Base64;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+/**
+ * 公钥私钥加密解密
+ */
+public class RSAEncryption {
+
+
+	private static byte[] PUB_KEY = null;
+	private static byte[] PRI_KEY = null;
+
+	// 数字签名，密钥算法
+	private static final String RSA_KEY_ALGORITHM = "RSA";
+
+	// 数字签名签名/验证算法
+	private static final String SIGNATURE_ALGORITHM = "MD5withRSA";
+
+	// RSA密钥长度，RSA算法的默认密钥长度是1024密钥长度必须是64的倍数，在512到65536位之间
+	private static final int KEY_SIZE = 1024;
+
+	private static void initKey() throws Exception {
+		KeyPairGenerator keygen = KeyPairGenerator.getInstance(RSA_KEY_ALGORITHM);
+		SecureRandom secureRandom = new SecureRandom();
+		secureRandom.setSeed("".getBytes());
+		keygen.initialize(KEY_SIZE, secureRandom);
+		KeyPair keyPair = keygen.genKeyPair();
+		PUB_KEY = keyPair.getPublic().getEncoded();
+		PRI_KEY = keyPair.getPrivate().getEncoded();
+		System.out.println("公钥 == " + Base64.encodeBase64String(PUB_KEY));
+		System.out.println("私钥 == " + Base64.encodeBase64String(PRI_KEY));
+
+	}
+
+	/**
+	 * RSA 加签
+	 *
+	 * @param data 待加签数据
+	 * @return byte[] 数字签名
+	 */
+	public static String sign(byte[] data) throws Exception {
+		if (PUB_KEY == null || PRI_KEY == null) {
+			initKey();
+		}
+		PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(PRI_KEY);
+		KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
+		PrivateKey priKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+		Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+		signature.initSign(priKey);
+		signature.update(data);
+		return Base64.encodeBase64String(signature.sign());
+	}
+
+	/**
+	 * 验签
+	 *
+	 * @param data 待校验数据
+	 * @param sign 数字签名
+	 * @return boolean 成功返回true,失败返回false
+	 */
+	public static boolean verify(byte[] data, byte[] sign) throws Exception {
+		if (PUB_KEY == null || PRI_KEY == null) {
+			initKey();
+		}
+		KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
+		X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(PUB_KEY);
+		PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+		Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+		signature.initVerify(publicKey);
+		signature.update(data);
+		return signature.verify(sign);
+	}
+
+	/**
+	 * 公钥加密数据
+	 *
+	 * @param data 待加密数据
+	 * @return byte[] 加密数据
+	 */
+	public static byte[] encryptByPubKey(byte[] data) throws Exception {
+		if (PUB_KEY == null || PRI_KEY == null) {
+			initKey();
+		}
+		X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(PUB_KEY);
+		KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
+		PublicKey publicKey = keyFactory.generatePublic(x509KeySpec);
+		Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		return cipher.doFinal(data);
+	}
+
+	/**
+	 * 公钥加密数据
+	 *
+	 * @param data 待加密数据
+	 * @return String 加密数据
+	 */
+	public static String encryptByPubKey(String data) throws Exception {
+		byte[] bytes = encryptByPubKey(data.getBytes());
+		return Base64.encodeBase64String(bytes);
+	}
+
+	/**
+	 * 私钥加密数据
+	 *
+	 * @param data 待加密数据
+	 * @return byte[] 加密数据
+	 */
+	public static byte[] encryptByPriKey(byte[] data) throws Exception {
+		if (PUB_KEY == null || PRI_KEY == null) {
+			initKey();
+		}
+		PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(PRI_KEY);
+		KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
+		PrivateKey privateKey = keyFactory.generatePrivate(pkcs8KeySpec);
+		Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+		cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+		return cipher.doFinal(data);
+	}
+
+	/**
+	 * 私钥加密数据
+	 *
+	 * @param data 待加密数据
+	 * @return String 加密数据
+	 */
+	public static String encryptByPriKey(String data) throws Exception {
+		byte[] enSign = encryptByPriKey(data.getBytes());
+		return Base64.encodeBase64String(enSign);
+	}
+
+	/**
+	 * 公钥解密
+	 *
+	 * @param data 待解密数据
+	 * @return byte[] 原始数据
+	 */
+	public static byte[] decryptByPubKey(byte[] data) throws Exception {
+		if (PUB_KEY == null || PRI_KEY == null) {
+			initKey();
+		}
+		X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(PUB_KEY);
+		KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
+		PublicKey publicKey = keyFactory.generatePublic(x509KeySpec);
+		Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+		cipher.init(Cipher.DECRYPT_MODE, publicKey);
+		return cipher.doFinal(data);
+	}
+
+	/**
+	 * 公钥解密
+	 *
+	 * @param data 待解密数据
+	 * @return String 原始数据
+	 */
+	public static String decryptByPubKey(String data) throws Exception {
+		byte[] design = decryptByPubKey(Base64.decodeBase64(data));
+		return new String(design);
+	}
+
+	/**
+	 * 私钥解密
+	 *
+	 * @param data 待解密数据
+	 * @return byte[] 原始数据
+	 */
+	public static byte[] decryptByPriKey(byte[] data) throws Exception {
+		if (PUB_KEY == null || PRI_KEY == null) {
+			initKey();
+		}
+		PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(PRI_KEY);
+		KeyFactory keyFactory = KeyFactory.getInstance(RSA_KEY_ALGORITHM);
+		PrivateKey privateKey = keyFactory.generatePrivate(pkcs8KeySpec);
+		Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+		cipher.init(Cipher.DECRYPT_MODE, privateKey);
+		return cipher.doFinal(data);
+	}
+
+	/**
+	 * 私钥解密
+	 *
+	 * @param data 待解密数据
+	 * @return String 原始数据
+	 */
+	public static String decryptByPriKey(String data) throws Exception {
+		byte[] design = decryptByPriKey(Base64.decodeBase64(data));
+		return new String(design);
+	}
+
+}
---
+++ happy-common/happy-common-utility/src/test/java/com/huifer/happy/common/utility/encryption/RSAEncryptionTest.java
@@ -0,0 +1,27 @@
+package com.huifer.happy.common.utility.encryption;
+
+
+import org.apache.commons.codec.binary.Base64;
+import org.junit.Test;
+
+
+public class RSAEncryptionTest {
+
+	@Test
+	public void rsaTest() throws Exception {
+		String data = "这是一段被加密的数据>.<";
+		// 公钥加密
+		String pubKeyStr = RSAEncryption.encryptByPubKey(data);
+		System.out.println("公钥加密后 == " + pubKeyStr);
+		// 私钥解密
+		String priKeyStr = RSAEncryption.decryptByPriKey(pubKeyStr);
+		System.out.println("私钥解密后 == " + priKeyStr);
+		// 数字签名验证
+		String sign1 = "sign1";
+		String sign = RSAEncryption.sign(sign1.getBytes());
+		System.out.println("数字签名结果 == " + sign);
+		// 数字签名验证
+		boolean verify = RSAEncryption.verify(sign1.getBytes(), Base64.decodeBase64(sign));
+		System.out.println(verify);
+	}
+}
