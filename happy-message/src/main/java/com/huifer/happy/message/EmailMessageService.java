538eb6d5b5f5d83046835181f0f53dab5cbbf9c2
huifer <1187982580@qq.com>
2019-08-31 20:06:55 +0800

    消息

---
+++ happy-message/src/main/java/com/huifer/happy/message/EmailMessageService.java
@@ -0,0 +1,13 @@
+package com.huifer.happy.message;
+
+import com.huifer.happy.interfaces.file.MessageInterface;
+
+/**
+ * 邮件发送服务
+ */
+public class EmailMessageService implements MessageInterface {
+    @Override
+    public String sendMessage(String from, String to, String msg) {
+        return null;
+    }
+}
