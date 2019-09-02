ee900fe319e3e17889f2bedeceb562c356d155d7
huifer <huifer97@163.com>
2019-09-02 13:18:41 +0800

    添加eureka-server

--- happy-security/src/main/java/com/huifer/happy/security/SecurityApplication.java
+++ happy-security/src/main/java/com/huifer/happy/security/SecurityApplication.java
@@ -2,9 +2,11 @@ package com.huifer.happy.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SecurityApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(SecurityApplication.class, args);
