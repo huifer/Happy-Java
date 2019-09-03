96aeede96b3f5158fa9bac781e1b627d43938c17
huifer <huifer97@163.com>
2019-09-03 22:37:02 +0800

    pass

--- happy-restfull-app/src/main/java/com/huifer/happy/restfull/controller/HeartController.java
+++ happy-restfull-app/src/main/java/com/huifer/happy/restfull/controller/HeartController.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,8 +27,10 @@ public class HeartController {
 	private DiscoveryClient discoveryClient;
 
 	@GetMapping("/hello")
-	public String hello() {
-		String zhangsan = securityFeign.hello("zhangsan");
+	public String hello(
+			@RequestParam("name") String name
+	) {
+		String zhangsan = securityFeign.hello(name);
 		return zhangsan;
 	}
 
--- happy-security/src/main/java/com/huifer/happy/security/controller/ApiController.java
+++ happy-security/src/main/java/com/huifer/happy/security/controller/ApiController.java
@@ -3,6 +3,7 @@ package com.huifer.happy.security.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
 
+	@Value("${server.port}")
+	private int port;
+
 	@ApiOperation(value = "hello", notes = "返回输入值")
 	@GetMapping("/hello")
 	public String hello(
 			@ApiParam(value = "姓名") @RequestParam("name") String name
 	) {
+		System.out.println("当前访问接口={}" + port);
 		return name;
 	}
 }
