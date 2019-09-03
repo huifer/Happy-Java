73314f4245c7110d5baf3e1472afe049a2b0d3ed
huifer <huifer97@163.com>
2019-09-03 09:39:17 +0800

    添加SpringBootAdmin和swagger

--- happy-security/readme.md
+++ happy-security/readme.md
@@ -1,3 +1,34 @@
 # spring boot 权限组件
 ## 基于 Oauth2.0
 ## 基于 spring-security
+
+## 新增配置文件
+```yaml
+eureka:
+  client:
+    registryFetchIntervalSeconds: 5
+    serviceUrl:
+      defaultZone: ${EUREKA_SERVICE_URL:http://localhost:8761}/eureka/
+  instance:
+    health-check-url-path: /actuator/health
+
+---
+management:
+  endpoints:
+    web:
+      exposure:
+        include: "*"
+  endpoint:
+    health:
+      show-details: ALWAYS
+---
+spring:
+  boot:
+    admin:
+      client:
+        url: "http://localhost:8084"
+        username: "admin"
+        password: "admin"
+        instance:
+          prefer-ip: true
+```
---
+++ happy-security/src/main/java/com/huifer/happy/security/controller/ApiController.java
@@ -0,0 +1,22 @@
+package com.huifer.happy.security.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(value = "测试api")
+@RestController
+public class ApiController {
+
+
+	@GetMapping("/hello")
+	@ApiOperation(value = "hello", notes = "返回输入值")
+	public String hello(
+			@ApiParam(value = "姓名") @RequestParam("name") String name
+	) {
+		return name;
+	}
+}
